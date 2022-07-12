2ed12361161113e5cf34f415ba75b048ec79d2d7
Danko Vracaric <Danko.Vracaric@rs.ey.com>
2022-07-12 12:57:32 +0200

    final commit

--- script.js
+++ script.js
@@ -1,12 +1,234 @@
-// Callbacks
+// function print1(){
+//     const number1 = 1;
+//     console.log(number1)
+// }
 
+// function print2(){
+//     function getNumber2(){
+//         return 2
+//     }
+//     const number2 = getNumber2()
+//     console.log(number2)
+// }
 
-// Promises
+// function print3(){
+//     const fs = require("fs");
+//     fs.readFile("./number3.txt", "utf-8", function (err, number3){
+//         console.log(number3)
+//     })
+// }
+// utf-9; human, normal readable text
 
+// function print4(){
+//     const number4 = 4;
+//     console.log(number4)
+// }
+
+// print1()
+// print2()
+// print3()
+// print4()
+
+// function print(number){
+//     console.log(number)
+// }
+
+// const numbers = [1,2,3,4]
+
+// numbers.forEach(print)
+
+// function f(cb){
+//     setTimeout(()=>cb(),0)
+// }
+
+// f(()=>console.log("This is a callback"))
+
+// console.log("Hello world!")
+
+// function f(cb){
+
+//     setTimeout(cb,0)
+// }
+
+// f(()=>console.log("This is a callback"))
+
+// console.log("Hello, world")
+
+// function calculateSquare(number,callback){
+//         setTimeout(function(){
+//             if(typeof number !== "number"){
+//                 throw new Error("Argument of type number is expected")
+//             }
+//         const result = number*number;
+//         callback(result);
+//         }, 1000)
+// }
+
+// try{
+//  calculateSquare("bad argument", function(){
+
+//         console.log(result, "ress")
+//  })
+// }catch(error){
+//  console.log("Caught error" + String(error))
+// }
+
+// I will get uncaught error, instead of caught errror we specifed
+//because setTimeout is a asynch function that will put our callback on the message que, while try catch block
+//resides outside
+
+// function calculateSquare(number,callback){
+//         setTimeout(function(){
+//             if(typeof number !== "number"){
+//                 callback(new Error("Argument of type number is expected"))
+//                 return;
+//             }
+//         const result = number*number;
+//         callback(null, result);
+//         }, 1000)
+// }
+
+// Let's modify the function in order to pass the error via arguments of the callback
+// we will not trow the error inside but invoke the callback and pass the error as the first argument
+
+// calculateSquare("bad argument", function(error, result){
+//     if(error !== null){
+//         console.log("Caught error:" + String(error))
+//         return;
+//     }
+//     console.log(result)
+// } )
+
+// calculateSquare(2, function(error, result){
+//     if(error !== null){
+//         console.log("Caught error:"+ String(error))
+//         return;
+//     }
+//     console.log(result)
+// })
+
+// CB examples in popular libraries
+
+// setState does not always immediately updates the state of the componenent, in batches several for performance reasons
+//so it is hard to say when the state will be actually updated
+// so setState allows to specify a cb, who will be invoked when state changes
+
+//in vanila js
+//btn.addEventListener("click",()=>{})
+
+//Callbacks lack of readability
+
+// DOG API
+//First I will get a first breed from the list and get a random picture of it
+// the important thing is we are going get these http requests one after another
+//second should start only after first finishes
+
+// const req = new XMLHttpRequest()
+
+// const method = "GET"
+
+// const url = "https://dog.ceo/api/breeds/list/all"
+
+// req.open(method, url)
+// req.onreadystatechange = function(){
+//     if(req.readyState === XMLHttpRequest.DONE){
+//         console.log(req.responseText)
+//         const res = JSON.parse(req.responseText)
+//         const breeds = Object.keys(res.message)
+//         console.log(breeds)
+//         const firstBreedInTheList= breeds[0];
+//         const req2 = new XMLHttpRequest()
+
+// const method2 = "GET"
+
+// const url2 = "https://dog.ceo/api/breeds/list/all"
+
+// req.open(method2, url2)
+// req.onreadystatechange = function(){
+//     if(req2.readyState === XMLHttpRequest.DONE){
+//         console.log(req.responseText)
+//         const res2 = JSON.parse(req2.responseText)
+//         const breeds = Object.keys(res.message)
+//         console.log(breeds)
+//         const firstBreedInTheList= breeds[0];
+
+//     }
+// }
+// req2.send(null)
+
+//     }
+// }
+// req2.send(null)
+
+// we need to handle the response as well
+// for this we are gonna use readyState() callback
+//every time XMLHttpRequest state proprety changes, readyState() is invoked
+
+//now we need to add a second http req after the first completes
+// we need to place code inside onreadystatechange cb
+
+// to make code more readable we can use promises
+
+// Callback Hell in JS
+
+// function calculateSquareNumm(number,callback){
+
+//     setTimeout(function(){
+
+//             if(typeof number !== "number"){
+//                 callback(new Error("Argument of type number is expected"))
+//                 return;
+//             }
+//             const result = number*number;
+//             callback(null, result)
+
+//     }, 1000)
+// }
+
+// print 3 results one after another
+// ono sto ce se desiti je da saceka 1 sekundu i ispise sva 3
+// calculateSquareNumm(1, function(error,result){
+//         if(error!==null){
+//             console.log("Caught" + String(error))
+//             return;
+//         }
+//         console.log(result)
+// })
+
+// calculateSquareNumm(2,(error, data)=>{
+
+//         if(error !== null){
+//             console.log("Caught"+ String(error))
+//             return;
+//         }
+//         console.log(data)
+// })
+// calculateSquareNumm(3,(error, data)=>{
+
+//         if(error !== null){
+//             console.log("Caught"+ String(error))
+//             return;
+//         }
+//         console.log(data)
+// })
+
+// calcSquare je asinhrona funk a mi je pozivamo sinhrono
+//js calculates each function should execute 1 second after it reaches it's line
+// so we need to move the execution of the second funk into the first func
+
+// calculateSquareNumm(1,function(error,result){
+//     console.log(result)
+//     calculateSquareNumm(2,function(error,result){
+//         console.log(result)
+//         calculateSquareNumm(3,function(erro,result){
+//             console.log(result)
+//         })
+//     })
+// })
 
 // 26. Handling promise rejections
 
-//when we want to handle erros separately from successful cases 
+//when we want to handle erros separately from successful cases
 //.then(undefined/null, onRejected)
 // function calculateSquare(number){
 
@@ -37,7 +259,7 @@
 // })
 // throw new Error("Something went wrong") it won't be caught if generated here, what we need to do is exctract onRejected func from this then(second then method and put it in third then method)
 //  rej=>{
-    // console.log(rej)
+// console.log(rej)
 // });
 // there is a drawback in this, if error happens in onResolved in first then, it will never be caught
 
@@ -79,7 +301,7 @@
 
 // const rejcted = Promise.reject(value)
 
-// we can use any value but documentation reccommends using instance of Error as a reason   
+// we can use any value but documentation reccommends using instance of Error as a reason
 
 // 28. Executing Promises in Parallel: Promise.all Method
 
@@ -95,7 +317,7 @@
 
 // function askFirstDealer(){
 //     return new Promise ((resolve, rej)=>{
-    
+
 //         setTimeout(()=>resolve(8000), 3000)
 //     })
 // }
@@ -129,7 +351,6 @@
 
 // Promise.all([1, "string", true]).then(res=>console.log(res))
 
-
 // 29. How Promise.all handles rejections
 //Promise.all rejects when the first promise rejects, it rejects with the reason of the first promise
 
@@ -137,7 +358,7 @@
 
 // function askFirstDealer(){
 //     return new Promise ((resolve, rej)=>{
-    
+
 //         setTimeout(()=>resolve(8000), 3000)
 //     })
 // }
@@ -169,16 +390,16 @@
 
 // this is possible because catch returns a promise and unless we throw inside the catch block, this promise will be resolved
 
-// Promise.all 
+// Promise.all
 // if we pass a promises that rejects immediatly, then Promise.all will reject immediately
 
 // Promise.all([askFirstDealer().catch(err=>err), askSecondDealer().catch(err=>err.message), askThirdDealer().catch(err=>err)], Promise.reject("rejected for some reason")).then(res=>console.log(res)).catch(rej=>{
-    // console.log(rej)
+// console.log(rej)
 // })
 
 // 30. Promise.race method, which Promise is the fastest
 
-// Let's imagine we are still going to school, one day we realize we forget our pen at home 
+// Let's imagine we are still going to school, one day we realize we forget our pen at home
 // I call my friends to see who has an extra pen, so the first person who answers me is all I need
 // I need to write others tnx I got the pen already
 
@@ -205,22 +426,17 @@
 //     })
 // }
 
-
-
 // now we need to run these results in paralled but only get the fastest result
 
 // Promise.race([askJohn(), askEugene(), askSusy()]).then(res=>console.log(res)).catch(err=>console.log(err))
 
 // it returns the fastest promise to RESOLVE OR REJECTS, so if Eugone rejected fastest I would never get a pen
 
-
 // Let's imagine there is a shop that sells pens, you can get and they respond instantly but you need to pay for it
-
 
 // var askTheShop = ()=>{
 //     return Promise.resolve("We always have pens, You can buy one for $1")
 // }
-
 
 // Promise.race([askJohn(), askEugene(), askSusy(), askTheShop()]).then(res=>console.log(res)).catch(err=>console.log(err))
 
@@ -261,5 +477,140 @@
 //     return Promise.reject(404)
 // }
 
-
 // 36. Await keyword in js
+
+// function getSpecificNumber(){
+//     return new Promise((res,rej)=>{
+//              setTimeout(()=>{res(42)}, 2000 )
+//     });
+// }
+
+// async function f(){
+//     const specificNumber = await getSpecificNumber();
+//     console.log(specificNumber);
+// }
+
+// var x = f()
+
+// 37. Using Async Await with Fetch API
+
+// It allows us to use HTTP requests and uses promises
+
+//fetch function returns respons object, you can invoke various methods on this object
+//in this case json, because I know that dogs api returns responses in json format
+//fetch returns response object
+// function getRandomDogImage(){
+//     fetch("https://dog.ceo/api/breeds/image/random").then(res=>res.json()).then(data=>console.log(data))
+// }
+
+// getRandomDogImage()
+// console.log("A")
+
+// In this case I will use await keyword to await for the response
+//basically we use await keyword on every place we used then
+// async function getRandomDogImage(){
+//     let response = await fetch("https://dog.ceo/api/breeds/image/random")
+//     let value = await response.json()
+//     console.log(value)
+// }
+
+// getRandomDogImage()
+// it's more readable, than chaining promises
+// await keyword doesn't work on top lvl
+
+// 37. Top Level Await
+// You can't use await inside regular function
+
+// function getSpecificNumber() {
+//   return new Promise((res, rej) => {
+//     setTimeout(() => rej(new Error("1234")), 2000);
+//   });
+// }
+
+// const specNumber = await getSpecificNumber();
+// console.log(specNumber)
+// (async function () {
+//   try {
+//     const specNumber = await getSpecificNumber();
+//     console.log(specNumber);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// })();
+
+//  38. Handling promise rejections using async await
+
+// If a promise gets rejected awaiting for this promise will throw an error
+// we can catch errors using try catch syntax
+
+// async function f() {
+//   try {
+//     const res = await fetch("https://www.fff.com/");
+//   } catch (err) {
+//     console.log("HEY" + err);
+//   }
+// }
+
+// f();
+
+//there is another way to handle rejected promises inside async function
+
+//every async func returns promise that means we can invoke .then and .catch on it
+
+// async function f() {
+//   const res = await fetch("https://www.fff.com/");
+// }
+
+// f().catch((err) => console.log(err));
+
+// 40. Sequential vs Parallel Execution
+
+//How we can execute multiple promises 1 by 1 and in paralel using async await!
+
+// function printNumber1() {
+//   return new Promise((res, rej) => {
+//     setTimeout(() => {
+//       console.log("Print1 is done!");
+//       res(1);
+//     }, 1000);
+//   });
+// }
+
+// function printNumber2() {
+//   return new Promise((res, rej) => {
+//     setTimeout(() => {
+//       console.log("Print2 is done!");
+//       res(2);
+//     }, 1000);
+//   });
+// }
+
+// now let's create a function that will invoke both one after another
+
+// async function oneByOne() {
+//   const number1 = await printNumber1();
+//   const number2 = await printNumber2();
+//   console.log(number1, number2);
+// }
+
+// oneByOne();
+
+// we can invoke these functions in parallel and wait until both of them are completed
+
+//   async function inParallel() {
+//     const promise1 = printNumber1();
+//     const promise2 = printNumber2();
+
+//     const number1 = await promise1;
+//     const number2 = await promise2;
+//     console.log(number1, number2);
+//   }
+
+//   inParallel();
+
+// use array destructuring and put two awaits in the same line
+// and still works
+
+// const [number1, number2] = [await promise1, await promise2]
+// I can also put await promise1, await promise 2 in console.log()
+//(you can use promise.all)
